03c80581aaa00ca763635ee12621c23a05ee6bb2
Shubham <shubhamkumarsharmaji@gmail.com>
2024-11-03 18:23:12 +0530

    twentee twoth commit

--- server.js
+++ server.js
@@ -1,33 +1,26 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const fs = require("fs").promises; // Use promises for non-blocking file operations
+const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-
-// Middleware to prevent caching
 app.use((req, res, next) => {
-  res.set("Cache-Control", "no-store");
+  res.set("Cache-Control", "no-store"); // Prevent caching
   next();
 });
 
 app.use(express.static("public")); // Serve static files
 app.use(cors());
-
 // MongoDB connection function
 async function connectToDatabase() {
   const uri = process.env.MONGODB_URI;
   try {
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(uri);
     console.log("MongoDB connected!");
   } catch (err) {
     console.error("Failed to connect to MongoDB:", err);
-    process.exit(1); // Exit the process if MongoDB connection fails
   }
 }
 
@@ -51,17 +44,16 @@ async function updateMatchJSON() {
   try {
     const matches = await Match.find();
     const jsonData = JSON.stringify(matches, null, 2);
-    await fs.writeFile(path.join(__dirname, "public/matches.json"), jsonData); // Non-blocking write
+    fs.writeFileSync(path.join(__dirname, "public/matches.json"), jsonData);
     console.log("Match data written to matches.json");
   } catch (error) {
     console.error("Error writing to JSON:", error);
   }
 }
-
-// Update match data and serve the main page
+// setInterval(updateMatchJSON, 5 * 60 * 1000);
 app.get("/", async (req, res) => {
-  await updateMatchJSON(); // Wait for the update to complete
-  const filePath = path.join(__dirname, "public", "matchPlayer.html");
+  await updateMatchJSON();
+  const filePath = path.join(__dirname, "public", "matchPlayer.html"); // Move up one level from Api folder
   console.log(`Serving file for HTML page is : ${filePath}`);
   res.sendFile(filePath, (err) => {
     if (err) {
@@ -71,7 +63,6 @@ app.get("/", async (req, res) => {
   });
 });
 
-// Serve directlyfile.html
 app.get("/directlyfile.html", (req, res) => {
   const filePath = path.join(__dirname, "public", "directlyfile.html");
   console.log(`Serving file for directlyfile page is : ${filePath}`);
