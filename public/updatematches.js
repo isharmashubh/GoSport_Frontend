a8072666f571d31e575c8bf6c124313a061ab9ee
Shubham <shubhamkumarsharmaji@gmail.com>
2024-11-03 21:50:21 +0530

    thiry fifth commit

---
+++ public/updatematches.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+const matchSchema = new mongoose.Schema({
+  name: String,
+  startTime: Date,
+  venue: String,
+  tour: String,
+  scorecard: String,
+  m3u8link: String,
+});
+
+const Match = mongoose.model("Match", matchSchema);
+async function updateMatchJSON() {
+  try {
+    const matches = await Match.find();
+    const jsonData = JSON.stringify(matches, null, 2);
+    console.log("Writing data inside matches.json");
+    fs.writeFileSync(path.join(__dirname, "matches.json"), jsonData);
+    console.log("Match data written to matches.json");
+  } catch (error) {
+    console.error("Error writing to JSON:", error);
+  }
+}
+
+updateMatchJSON().catch((error) => {
+  console.error("Failed to update matches.json:", error);
+});
